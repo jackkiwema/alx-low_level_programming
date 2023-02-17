d3565fda91515e0f7c3b3143963178dcb0b5f5a2
jackkiwema <jackkiwema@gmail.com>
2023-02-17 05:56:40 -0800

    Added 4

--- 0x01-variables_if_else_while/4-print_alphabet.c
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -2,20 +2,23 @@
 
 /**
  * main - program entrypoint
+ *
  * Description - print lowercase alphabet letters except q and e
+ *
  * Return: return 0 if success
  */
 
 int main(void)
 {
-	for (int i = 97; i <= 122; i++)
+	int i = 97;
+
+	while (i <= 122)
 	{
+		if (i != 101 && i != 113)
 		{
-			if (i != 101 && i != 113)
-			{
-				putchar(i);
-			}
+			putchar(i);
 		}
+		i++;
 	}
 	putchar('\n');
 
