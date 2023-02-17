6b65a7d65f0f11407c134fb5c30493b48753d95e
jackkiwema <jackkiwema@gmail.com>
2023-02-17 06:06:40 -0800

    Added 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,20 @@
 
 /**
  * main - program entry point
+ *
  * Description - print 0 - 9 numerals
+ *
  * Return: return 0 if success
  */
 
 int main(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i = 0;
+
+	while (i <= 9)
 	{
 		printf("%d", i);
+		i++;
 	}
 	printf("\n");
 
