080a43a323836ec05606f0fbf1189ae38bc89586
jackkiwema <jackkiwema@gmail.com>
2023-02-17 08:26:54 -0800

    Added 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * main - program entrypoint
+ *
+ * Description - print all possible tripple digits
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i = 48, j, k;
+
+	while (i <= 57)
+	{
+		j = i + 1;
+
+		while (j <= 57)
+		{
+			k = j + 1;
+
+			while (k <= 57)
+			{
+				if (!(i == 48 && j == 48 && k == 48))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i < 55 || j < 56 || k < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+
+				}
+
+				k++;
+
+			}
+
+			j++;
+
+		}
+
+		i++;
+
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
