0ee40210507090b3653abe1509c951be1e8a3eaf
jackkiwema <jackkiwema@gmail.com>
2023-02-17 09:12:45 -0800

    Added 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - program entrypoint
+ *
+ * Description - prints double array of numbers from 00 - 99
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i = 0, j = 0;
+
+	while (i <= 99)
+	{
+		j = i + 1;
+
+		while (j <= 99)
+		{
+		
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(32);
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (i != 98 || j != 99)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
