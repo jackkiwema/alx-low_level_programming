3a5f66fee9d87d764bb20754b59020e85516a99a
jackkiwema <jackkiwema@gmail.com>
2023-02-21 11:16:42 -0800

    Added 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,10 +9,11 @@
 void jack_bauer(void)
 {
 	int hours = 0;
-	int minutes = 0;
+	int minutes;
 
 	while (hours < 24)
 	{
+		minutes = 0;
 		while (minutes < 60)
 		{
 			_putchar((hours / 10) + '0');
@@ -21,8 +22,8 @@ void jack_bauer(void)
 			_putchar((minutes / 10) + '0');
 			_putchar((minutes % 10) + '0');
 			_putchar('\n');
+			minutes++;
 		}
-		minutes++;
+		hours++;
 	}
-	hours++;
 }
