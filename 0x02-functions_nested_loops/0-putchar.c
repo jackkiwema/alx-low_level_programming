57b0c4e7451b5ecb535b44cd578aa8176e29044e
jackkiwema <jackkiwema@gmail.com>
2023-02-21 09:03:00 -0800

    rectified spaces

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	char text[8] = "_putchar";
+	char text[10] = "_putchar";
 
 	int i = 0;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(text[i]);
 	}
