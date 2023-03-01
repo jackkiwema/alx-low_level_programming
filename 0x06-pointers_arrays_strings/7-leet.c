d55e81c359560ebf5367a90b9205ed8d9367169b
jackkiwema <jackkiwema@gmail.com>
2023-03-01 12:27:24 -0800

    Added 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,25 +3,23 @@
 /**
  * leet - function that encodes a string into 1337
  * @s: string array
- * Retrun: string array
+ * Return: string array
  */
 
 char *leet(char *s)
 {
-	int i;
+	int i = 0, j;
+	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j <= 7; j++)
+		{
+			if (s[i] == leet[j] || s[i] - 32 == leet[j])
+				str[i] = j + '0';
+		}
+
+		i++;
 	}
 
 	return (s);
