5fe325fd8ac5d32e1abd7faef058e42f34e0fba0
jackkiwema <jackkiwema@gmail.com>
2023-03-01 12:55:47 -0800

    Added 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,38 +7,16 @@
 
 void print_number(int n)
 {
-	unsigned int abs;
-	int mult = 1;
-	unsigned int abscount;
-	int i;
-	int c = 0;
+	unsigned int num = n;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	}
 	if (n < 0)
 	{
 		_putchar('-');
-		n += 1;
-		n *= -1;
-		n++:
+		num = -num;
 	}
-	abs = n;
-	abscount = n;
 
-	while (abscount > 0)
-	{
-		abscount /= 10;
-		c++;
-	}
-	for (i = 0; i < c - 1; i++)
-		mult *= 10;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	for (i = 0; i < c; i++)
-	{
-		_putchar((abs / mult) + '0');
-		abs = abs % mult;
-		mult /= 10;
-	}
+	_putchar((num % 10) + '0');
 }
